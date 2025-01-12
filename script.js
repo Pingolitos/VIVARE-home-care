db08c076d80f720c6e72fac2fb84f94f6464c915
Pingolitos <00001132500643sp@al.educacao.sp.gov.br>
2025-01-12 13:41:05 -0300

    Create script.js
    Script JS criado

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simples alerta ao enviar o formulário
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Obrigado por entrar em contato! Retornaremos em breve.');
+});
